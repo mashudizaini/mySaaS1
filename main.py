6ffcf03c46177078df1b5bd00eb7e00e8d6fcbda
mashudizaini <46367640+mashudizaini@users.noreply.github.com>
2025-05-09 16:37:12 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os 
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello SaaS!"}
+
+# Gunakan PORT dari environment variable Railway
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
